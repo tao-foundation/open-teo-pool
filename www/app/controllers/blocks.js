a255875b31d7fa87dd1baf2f28a46d6cfc1d12a4
hackyminer <hackyminer@gmail.com>
2018-10-09 10:30:50 +0900

    update charts, more configurable options

--- www/app/controllers/blocks.js
+++ www/app/controllers/blocks.js
@@ -56,32 +56,69 @@ export default Controller.extend({
                             second: "%H:%M:%S",
                             minute: "%H:%M",
                             hour: "%H:%M",
-                            day: "%e. %b",
-                            week: "%e. %b",
+                            day: "%m/%d",
+                            week: "%m/%d",
                             month: "%b '%y",
                             year: "%Y"
                         }
                     },
-                    yAxis: {
+                    yAxis: [{
                         labels: {
                             style: {
                                 color: '#6e6e70'
+                            },
+                            formatter: function() {
+                                var f = this.value;
+                                var units = ['H', 'KH', 'MH', 'GH', 'TH'];
+                                for (var i = 0; i < 5 && f > 1000; i++)  {
+                                    f /= 1000;
+                                }
+                                return f.toFixed(2) + ' ' + units[i];
                             }
                         },
                         title: {
-                            text: "shares and difficulty",
+                            text: "Difficulty",
                             style: {
                                 color: 'black',
-                                fontSize: '16px',
-                                fontWeight: 'bold'
+                                fontSize: '13px',
+                                fontWeight: 'normal'
                             }
                         },
                         softMax: 100,
-                    },
-                    plotLines: [{
-                        value: 0,
-                        width: 1,
-                        color: "#808080"
+                        gridLineColor: "#e6e6e6"
+                    }, {
+                        labels: {
+                            style: {
+                                color: '#6e6e70'
+                            },
+                            formatter: function() {
+                                return this.value.toFixed(0) + ' %';
+                            }
+                        },
+                        title: {
+                            text: "Luck",
+                            style: {
+                                color: 'black',
+                                fontSize: '13px',
+                                fontWeight: 'normal'
+                            }
+                        },
+                        opposite: true,
+                        softMax: 100,
+                        gridLineColor: "#e6e6e6",
+
+                        plotLines: [{
+                            value: 100,
+                            width: 2,
+                            color: "#4398de",
+                            label: {
+                            text: 'expected: 100 %',
+                            align: 'center',
+                            style: {
+                              color: 'gray'
+                            }
+                          }
+                       }]
                     }],
                     plotOptions: {
                         series: {
@@ -95,84 +132,102 @@ export default Controller.extend({
                         }
                     },
                     legend: {
-                        enabled: true
+                        enabled: true,
+                        itemStyle: {}
                     },
                     tooltip: {
                         formatter: function() {
-                            var ss = this.y > 1000000000000 ? "<b>" + (this.y / 1000000000000).toFixed(2) + "&nbsp;TH</b>" : this.y > 1000000000 ? "<b>" + (this.y / 1000000000).toFixed(2) + "&nbsp;GH</b>" : this.y > 1000000 ? "<b>" + (this.y / 1000000).toFixed(2) + "&nbsp;MH</b>" : this.y > 1000 ? "<b>" + (this.y / 1000).toFixed(2) + "&nbsp;KH</b>" : "<b>" + this.y.toFixed(2) + "&nbsp;H</b>";
-                            return ss + "<br/><b>Number:&nbsp;" + this.point.h + "</b><br/><b>" + this.point.d + "</b><br/><b>Reward:&nbsp;" + (this.point.w/1000000000000000000).toFixed(8) + e.get('config.Unit') + " </b><br/><b>Variance:&nbsp;" + (this.point.s*100).toFixed(2)+ "%</b>";
+                            function scale(v) {
+                                var f = v;
+                                var units = ['H', 'KH', 'MH', 'GH', 'TH'];
+                                for (var i = 0; i < 5 && f > 1000; i++)  {
+                                    f /= 1000;
+                                }
+                                return f.toFixed(2) + ' ' + units[i];
+                            }
+
+                            var d = scale(this.point.f);
+                            var s = scale(this.point.s);
+
+                            return "<div>" +
+                              "<b>Difficulty: " + d + "</b><br/>" +
+                              "<b>Shares: " + s + "</b><br/>" +
+                              "<b>Luck: " + (this.point.s*100).toFixed(2)+ " %</b><br/>" +
+                              (this.point.w > 0 ? "<b>Reward:&nbsp;" + (this.point.w/1000000000000000000).toFixed(6) + ' ' + e.get('config.Unit') + "</b><br/>" : '') +
+                              "<b>Block Height: #" + this.point.h + "</b><br/>" +
+                              "<b>" + this.point.d + "</b><br/>" +
+                              "</div>";
                         },
 
                         useHTML: true
                     },
                     exporting: {
-                        enabled: false
+                        enabled: true
                     },
                     series: [{
+                        yAxis: 0,
                         step: 'center',
                         color: "#E99002",
                         name: "difficulty",
                         data: function() {
-                            var e, a = [];
+                            var a = [];
                             if (null != t) {
-                                for (e = 0; e <= t.length - 1; e += 1) {
-                                    var n = 0,
-                                        r = 0,
-                                        l = 0;
-                                        r = new Date(1e3 * t[e].x);
-                                        l = r.toLocaleString();
-                                        n = t[e].difficulty;
-                                        a.push({
-                                            x: r,
-                                            d: l,
-                                            h: t[e].height,
-                                            w: t[e].reward,
-                                            s: t[e].sharesDiff,
-                                            y: n
-                                        });
-                                }
-                            } else {
-                                a.push({
-                                    x: 0,
-                                    d: 0,
-                                    y: 0
+                                t.forEach(function(i) {
+                                    var n = 0, r = 0, l = 0;
+                                    r = new Date(1e3 * i.x);
+                                    l = r.toLocaleString();
+                                    n = i.difficulty;
+                                    a.push({
+                                        x: r, d: l, y: n,
+                                        h: i.height, w: i.reward, s: i.sharesDiff, f: i.difficulty
+                                    });
                                 });
+                            } else {
+                                a.push({ x: 0, d: 0, y: 0, h: 0, w: 0, s: 0, f: 0 });
                             }
                             return a;
                         }()
                     }, {
+                        yAxis: 1,
                         step: 'center',
-                        name: "shares",
+                        color: "#3db72f",
+                        name: "Luck",
                         data: function() {
-                            var e, a = [];
+                            var a = [];
                             if (null != t) {
-                                for (e = 0; e <= t.length - 1; e += 1) {
-                                    var n = 0,
-                                        r = 0,
-                                        l = 0;
-                                        r = new Date(1e3 * t[e].x);
-                                        l = r.toLocaleString();
-                                        n = t[e].shares;
-                                        a.push({
-                                            x: r,
-                                            d: l,
-                                            h: t[e].height,
-                                            w: t[e].reward,
-                                            s: t[e].sharesDiff,
-                                            y: n
-                                        });
-                                }
-                            } else {
-                                a.push({
-                                    x: 0,
-                                    d: 0,
-                                    y: 0
+                                t.forEach(function(i) {
+                                    var n = 0, r = 0, l = 0;
+                                    r = new Date(1e3 * i.x);
+                                    l = r.toLocaleString();
+                                    n = i.sharesDiff * 100;
+                                    a.push({
+                                        x: r, d: l, y: n,
+                                        h: i.height, w: i.reward, s: i.sharesDiff, f: i.difficulty
+                                    });
                                 });
+                            } else {
+                                a.push({ x: 0, d: 0, y: 0, h: 0, w: 0, s: 0, f: 0 });
                             }
                             return a;
                         }()
                     }]
                 };
+            a.title.text = this.getWithDefault('config.highcharts.blocks.title', '');
+            a.chart.height = this.getWithDefault('config.highcharts.blocks.height', 200);
+
+            a.chart.backgroundColor = this.getWithDefault('config.highcharts.blocks.backgroundColor', "transparent");
+            a.xAxis.lineColor = this.getWithDefault('config.highcharts.blocks.lineColor', "#ccd6eb");
+            a.yAxis.lineColor = this.getWithDefault('config.highcharts.blocks.lineColor', "#ccd6eb");
+            a.xAxis.tickColor = this.getWithDefault('config.highcharts.blocks.tickColor', "#ccd6eb");
+            a.yAxis.tickColor = this.getWithDefault('config.highcharts.blocks.tickColor', "#ccd6eb");
+            a.xAxis.gridLineColor = this.getWithDefault('config.highcharts.blocks.gridLineColor', "#ccd6eb");
+            a.xAxis.gridLineWidth = this.getWithDefault('config.highcharts.blocks.gridLineWidthX', "0");
+            a.yAxis[0].gridLineColor = this.getWithDefault('config.highcharts.blocks.gridLineColor', "#ccd6eb");
+            a.yAxis[1].gridLineColor = this.getWithDefault('config.highcharts.blocks.gridLineColor', "#ccd6eb");
+            a.yAxis[0].title.style.color = this.getWithDefault('config.highcharts.blocks.labelColor', 'black');
+            a.yAxis[1].title.style.color = this.getWithDefault('config.highcharts.blocks.labelColor', 'black');
+            a.yAxis[1].plotLines[0].color = this.getWithDefault('config.highcharts.blocks.plotLineColor', '#4398de');
+            a.legend.itemStyle.color = this.getWithDefault('config.highcharts.blocks.labelColor', "#fff");
             return a;
         }
     })
