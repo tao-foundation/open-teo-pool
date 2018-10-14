0e765a135b9ca68ecedebeddf1c875665b40bfc1
hackyminer <hackyminer@gmail.com>
2018-10-15 05:43:24 +0900

    newStyle theme added

--- www/app/controllers/payments.js
+++ www/app/controllers/payments.js
@@ -4,5 +4,34 @@ import { computed } from '@ember/object';
 
 export default Controller.extend({
   applicationController: inject('application'),
-  config: computed.reads('applicationController.config')
+  stats: computed.reads('applicationController'),
+  settings: computed.reads('applicationController.model.settings'),
+  config: computed.reads('applicationController.config'),
+
+  PaymentsAmount: computed({
+    get() {
+      return (parseFloat(this.get('model').paymentsAmount) / 1000000000);
+    }
+  }),
+
+  earnPerDay: computed('stats', {
+    // earn per day per 1 GH/s
+    get() {
+      let reward = this.getWithDefault('stats.blockReward', this.get('config').BlockReward);
+      let blocktime = this.getWithDefault('stats.blockTime', this.get('config').BlockTime);
+      return (24 * 60 * 60 / blocktime * reward * 1000000000 / this.get('stats.hashrate')).toFixed(2);
+    }
+  }),
+
+  // try to read some settings from the model.settings
+  PayoutThreshold: computed('settings', {
+    get() {
+      var threshold = this.get('settings.PayoutThreshold');
+      if (threshold) {
+        // in shannon (10**9)
+        return threshold / 1000000000;
+      }
+      return this.get('config').PayoutThreshold;
+    }
+  })
 });
