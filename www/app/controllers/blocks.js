012cc1f462d869dff8728a5e894bc68beb9f333b
hackyminer <hackyminer@gmail.com>
2018-10-15 05:24:52 +0900

    controllers/blocks: fixed partial chart update (FIXME)

--- www/app/controllers/blocks.js
+++ www/app/controllers/blocks.js
@@ -4,8 +4,10 @@ import { computed } from '@ember/object';
 
 export default Controller.extend({
   applicationController: inject('application'),
+  stats: computed.reads('applicationController'),
   config: computed.reads('applicationController.config'),
   settings: computed.reads('applicationController.model.settings'),
+  lastData: null,
 
   BlockUnlockDepth: computed('settings', {
     get() {
@@ -17,10 +19,10 @@ export default Controller.extend({
     }
   }),
 
-  chartOptions: computed("model.luckCharts", {
+  chartOptions: computed("model.luckCharts", 'stats', {
         get() {
             var e = this,
-                t = e.getWithDefault("model.luckCharts"),
+                t = e.getWithDefault("model.luckCharts", []),
                 a = {
                     colors: ['#f45b5b', '#8085e9', '#8d4654', '#7798BF', '#aaeeee',
                             '#ff0066', '#eeaaee', '#55BF3B', '#DF5353', '#7798BF', '#aaeeee'],
@@ -30,12 +32,90 @@ export default Controller.extend({
                         height: 200,
                         events: {
                             load: function() {
-                                var series = this.series[0];
-                                setInterval(function() {
-                                    var x = (new Date()).getTime(),
-                                        y = e.getWithDefault("model.luckCharts.difficulty");
-                                    series.addPoint([x, y], true, true);
-                                }, 1090000000);
+                                var series = this.series;
+
+                                var chartInterval = setInterval(function() {
+                                    if (!series || !series[0]) {
+                                        clearInterval(chartInterval);
+                                        return;
+                                    }
+                                    var now = new Date();
+                                    t = e.getWithDefault("model.luckCharts", []);
+                                    // save lastData or reload LastData
+                                    var lastData = e.get('lastData');
+                                    if (!lastData) {
+                                        lastData = series[0].data[series[0].data.length - 1];
+                                        e.set('lastData', lastData);
+                                    }
+                                    var dataLast = t && t.length ? t[t.length - 1] : null;
+
+                                    var check = dataLast ? dataLast.x - parseInt(lastData.x / 1000) : 0;
+                                    check = check < 0 ? -check : check;
+                                    if (check < 5) {
+                                        // check datapoint shift
+                                        var shift = false;
+                                        if (series[0].data.length > 2 && now - series[0].data[0].x > 15*24*60*60*1000) {
+                                            // show 15 days
+                                            shift = true;
+                                        }
+
+                                        // partially update chart
+                                        if (series[0].data.length > 1 && series[0].data[series[0].data.length - 1].x - series[0].data[series[0].data.length - 2].x < 5*60*1000) {
+                                            // remove temporary added point
+                                            series[0].removePoint(series[0].data.length - 1, false, false);
+                                            series[1].removePoint(series[1].data.length - 1, false, false);
+                                        }
+
+                                        // simply update the last point
+                                        var l = now.toLocaleString();
+                                        var n = e.get('stats.difficulty');
+                                        var shareDiff = e.get('stats.roundShares') / n;
+                                        var height = e.get('stats.height');
+                                        var point = {
+                                            x: now, d: l, y: parseInt(n),
+                                            h: height, w: 0, s: shareDiff, f: n
+                                        };
+
+                                        var luck = {};
+                                        Object.assign(luck, point);
+                                        luck.y = shareDiff * 100;
+
+                                        // update chart
+                                        series[0].addPoint(point, false, shift);
+                                        // add and redraw
+                                        series[1].addPoint(luck, true, shift);
+                                        return;
+                                    }
+
+                                    // new data found, redraw charts using updated data
+                                    var diffs = [];
+                                    t.forEach(function(d) {
+                                        var r = new Date(1000 * d.x);
+                                        var l = r.toLocaleString();
+                                        var n = d.difficulty;
+                                        diffs.push({
+                                            x: r, d: l, y: n,
+                                            h: d.height, w: d.reward, s: d.sharesDiff, f: d.difficulty
+                                        });
+                                    });
+                                    var lucks = [];
+                                    t.forEach(function(d) {
+                                        var r = new Date(1000 * d.x);
+                                        var l = r.toLocaleString();
+                                        var n = d.sharesDiff * 100;
+                                        lucks.push({
+                                            x: r, d: l, y: n,
+                                            h: d.height, w: d.reward, s: d.sharesDiff, f: d.difficulty
+                                        });
+                                    });
+                                    if (diffs.length > 0) {
+                                        series[0].setData(diffs, true, {}, true);
+                                        e.set('lastData', diffs[diffs.length - 1]); // update lastData
+                                    }
+                                    if (lucks.length > 0) {
+                                        series[1].setData(lucks, true, {}, true);
+                                    }
+                                }, e.getWithDefault('config.highcharts.blocks.interval', 10000));
                             }
                         }
                     },
